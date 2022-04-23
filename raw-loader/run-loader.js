ffcd4f8c650d9a001a8542a5b2dc842ddcfdb2e0
ronnycyy <m17722548276@163.com>
2022-04-23 19:01:20 +0800

    异步 loader

--- raw-loader/run-loader.js
+++ raw-loader/run-loader.js
@@ -7,7 +7,13 @@ const path = require('path');
 runLoaders({
   resource: path.resolve(__dirname, 'src/demo.txt'),
   loaders: [
-    path.join(__dirname, './src/raw-loader.js')
+    {
+      loader: path.join(__dirname, './src/raw-loader.js'),
+      options: {
+        id: 111,
+        name: 'test',
+      }
+    }
   ],
   context: {
     minimize: true
--- raw-loader/src/raw-loader.js
+++ raw-loader/src/raw-loader.js
@@ -1,12 +1,29 @@
 // 将 文件内容 转换为 模块
 
+const loaderUtils = require('loader-utils');
+const fs = require('fs');
+const path = require('path');
+
 // loader 是一个函数
 module.exports = function (source) {
+
+  // 获取用户传给本 loader 的参数
+  const option = loaderUtils.getOptions(this);
+
+  // 异步 loader 回调
+  const callback = this.async();
+
+  // console.log('name', option);
 
   const json = JSON.stringify(source)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2028/g, '\\u2029')
 
+  // return `export default ${json}`;
 
-  return `export default ${json}`;
+
+  fs.readFile(path.resolve(__dirname, './async.txt'), 'utf-8', (err, data) => {
+    // 异步 loader
+    callback(null, data);
+  });
 }
